fc1b39822cb007e00f17e2cc9e2f2804c2512857
Viola Ceni <viola.ceni@cit.edu.al>
2019-10-16 14:25:18 +0200

    defined routes for post dsiplay

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'PostController@all');
+Route::get('/posts/{post}', 'PostController@single');
