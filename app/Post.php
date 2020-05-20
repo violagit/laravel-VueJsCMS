29607d853a4b0104f25ab9abdc95cda39f36e561
Viola Ceni <viola.ceni@cit.edu.al>
2020-05-20 14:55:10 +0200

    fix(api):edited routes

--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,8 @@ use App\User;
 class Post extends Model
 {
     protected $fillable = ['user_id', 'title', 'body', 'image'];
+
+    //add relationship
     public function user()
     {
         return $this->belongsTo(User::class);
